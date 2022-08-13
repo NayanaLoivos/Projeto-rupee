25371ba04b677a7733ac047bb314cf3531772696
Nayana Loivos <loivosdev@gmail.com>
2022-08-13 07:18:06 -0300

    Cria um novo project com tratamento de erros

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,23 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end  
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render :new
+    end
+  end
+  
+  private
+  def project_params 
+    params.require(:project).permit(:titulo, :descricao, :duracao)
+  end  
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
     #...herda de < ......
+    validates :titulo, presence: true, length: { minimum: 5 }
+    validates :descricao, presence: true, length: { minimum: 10 }
+    validates :duracao, presence: true
 end
