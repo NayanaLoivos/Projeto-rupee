dc970bd25938beb0f23e3be0577908d5b6b12f7d
Nayana Loivos <loivosdev@gmail.com>
2022-08-13 23:30:55 -0300

    modelo de user básico incluindo um password seguro com bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_14_003042) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_14_013540) do
   create_table "projects", force: :cascade do |t|
     t.string "titulo"
     t.text "descricao"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_14_003042) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup #executa before de cada test
-    @user = User.new(name: 'Example user', email: 'user@example.com')
+    @user = User.new(name: 'Example user', email: 'user@example.com',
+                    password: 'foobar', password_confirmation: 'foobar')
+
+    
   end
 
   test 'should be valid' do 
@@ -52,6 +55,16 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
   # test "the truth" do
   #   assert true
