45c8dea2fea4c649176e102e5bc8596f4220bdcc
Nayana Loivos <loivosdev@gmail.com>
2022-08-14 11:40:57 -0300

    Formulario de cadastro de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'projects#index' #raiz da rota
 
   resources :projects
-#controller_name projects#action_index
+  resources :users
+  #controller_name projects#action_index
 end
