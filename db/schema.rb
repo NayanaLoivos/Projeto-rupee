b73ea094b2bf50f0550661f4e0dcdb7243be26e7
Nayana Loivos <loivosdev@gmail.com>
2022-08-13 22:06:29 -0300

    test de  user e email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_193859) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_14_003042) do
   create_table "projects", force: :cascade do |t|
     t.string "titulo"
     t.text "descricao"
     t.integer "duracao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
